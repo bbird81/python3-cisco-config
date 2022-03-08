9bb2361ade3b9fee85acba0dc10a7691ea905c32
bbird81 <thomasguz@gmail.com>
2022-03-08 17:01:05 +0100

    refactor completo

--- inline_conf_run.py
+++ inline_conf_run.py
@@ -3,7 +3,7 @@
 ver = "python3-cisco-config ver. 1.5.0i - 2022-03-06 | https://github.com/ste-giraldo"
 
 # All pre-installed besides Netmiko.
-from csv import reader
+import csv
 from datetime import date, datetime
 from netmiko import ConnectHandler
 from netmiko import ssh_exception, Netmiko
@@ -11,6 +11,7 @@ from paramiko.ssh_exception import AuthenticationException
 from netmiko.ssh_exception import NetMikoAuthenticationException
 from ping3 import ping, verbose_ping 
 import getpass, os, os.path, sys, getopt, time, cmd
+import socket
 
 class bcolors:
     HEADER = '\033[95m'
@@ -33,15 +34,74 @@ if not os.path.exists('result-config'):
 now = datetime.now()
 dt_string = now.strftime("%Y-%m-%d_%H-%M")
 
-# Gives us the information we need to connect.
-def get_saved_config_host(host, username, password, enable_secret):
-    cisco_ios = {
-        'device_type': 'cisco_ios',
-        'host': host,
-        'username': username,
-        'password': password,
-        'secret': enable_secret,
-    }
+def check_port(ip):
+    '''
+    Verifica quale porta fra ssh-telnet è aperta e restituisce il nome del driver corrispondente (con preferenza su ssh)
+    '''
+    cp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    ip_ssh = (ip, 22)
+    ip_telnet = (ip, 23)
+    try:
+        check = cp.connect_ex(ip_ssh)
+        if check == 0:
+            return 'cisco_ios'
+    except: #in case of closed ssh, telnet will be tested
+        try:
+            check = cp.connect_ex(ip_telnet)
+            if check == 0:
+                return 'cisco_ios_telnet'
+        except:
+            return '' #no port is open, implement raise exception and continue
+
+def print_help():
+    '''
+    Just prints the help message
+    '''
+    print(ver)
+    print()
+    print('Usage: conf_run.py -c <config_filename> -s <host_list.csv> (Opt --verbose)')
+    print('Note: Default output filename is DNS based (check README.md)')
+    print()
+    print('       -c, --conf <config_filename>')
+    print('       -s, --csv <host_list.csv>')
+    print('       Optional -v, --verbose')
+    print('       Optional -n, --host    Output filename use hostname retrived from device')
+    #print('       Device connection method: --ssh (SSH: default), --tnet (telnet)')
+    print('       -h, --help    Print this help and exit')
+
+def test_devices():
+    '''
+    Reads the CSV file, writes a file with non-pingable devices and returns a list of active_devices
+    '''
+    active_devices = []
+    with open(csv_name, 'r') as csvfile:
+        csv_reader = csv.DictReader(csvfile)
+        for ip in csv_reader['IP']:
+            ip_ping = ping(ip)
+            if ip_ping == None:
+                fileName = "downDevices_" + dt_string + ".txt"
+                downDeviceOutput = open("result-config/" + fileName, "a")
+                downDeviceOutput.write(str(ip) + "\n")
+                print(str(ip) + " is down!")
+            else: active_devices.append(ip)
+    return active_devices
+
+def get_saved_config(host, username, password, enable_secret, flag_host):
+    '''
+    Executes commands and saves config file
+    '''
+    try:
+        cisco_ios = {
+            'device_type': check_port(host), #checks whichever port 22-23 is open and returns name of corresponding netmiko driver
+            'host': host,
+            'username': username,
+            'password': password,
+            'secret': enable_secret
+        }
+    except:
+        #gestione del caso in cui entrambe le porte ssh/telnet sono chiuse
+        print('Error creating socket: ssh and telnet closed')
+        sys.exit(2)
     # Creates connection to the device.
     try:
         net_connect = ConnectHandler(**cisco_ios)
@@ -52,12 +112,15 @@ def get_saved_config_host(host, username, password, enable_secret):
         print()
         print(output)
         print()
-        # Gets and splits the hostname for the output file name.
-        hostname = net_connect.send_command("show ver | i uptime")
-        hostname = hostname.split()
-        hostname = hostname[0]
-        # Creates the file name with hostname and date and time.
-        fileName = hostname + "_" + dt_string
+        # Creates the file name with either hostname/ip and date and time.
+        if flag_host:
+            # Gets and splits the hostname for the output file name.
+            hostname = net_connect.send_command("show ver | i uptime")
+            hostname = hostname.split()
+            hostname = hostname[0]
+            fileName = hostname + "_" + dt_string
+        else:
+            fileName = host + "_" + dt_string
         # Creates the text file in the result-config folder with the special name and writes to it.
         backupFile = open("result-config/" + fileName + ".txt", "w+")
         backupFile.write(output)
@@ -65,202 +128,22 @@ def get_saved_config_host(host, username, password, enable_secret):
     # Handle an authentication error.
     except (AuthenticationException, NetMikoAuthenticationException):
         print("Login failed " + host)
-
-def get_saved_config_dns(host, username, password, enable_secret):
-    cisco_ios = {
-        'device_type': 'cisco_ios',
-        'host': host,
-        'username': username,
-        'password': password,
-        'secret': enable_secret,
-    }
-    # Creates the connection to the device.
-    try:
-        net_connect = ConnectHandler(**cisco_ios)
-        net_connect.enable()
-        # Configuring from commands in variable config file.
-        output = net_connect.send_config_from_file(conf_name)
-        time.sleep(0.5)
-        print()
-        print(output)
-        print()
-        # Creates the file name, which is the hostname, and the date and time.
-        fileName = ip + "_" + dt_string
-        # Creates the text file in the backup-config folder with the special name, and writes to it.
-        backupFile = open("result-config/" + fileName + ".txt", "w+")
-        backupFile.write(output)
-        print("Outputted to " + fileName + ".txt")
-    # Handle an authentication error.
-    except (AuthenticationException, NetMikoAuthenticationException):
-        print("Login failed " + host)
-
-def get_saved_config_host_tnet(host, username, password, enable_secret):
-    cisco_ios = {
-        'device_type': 'cisco_ios_telnet',
-        'host': host,
-        'username': username,
-        'password': password,
-        'secret': enable_secret,
-    }
-    # Creates connection to the device.
-    try:
-        net_connect = ConnectHandler(**cisco_ios)
-        net_connect.enable()
-        # Configuring from commands in variable config file.
-        output = net_connect.send_config_from_file(conf_name)
-        time.sleep(0.5)
-        print()
-        print(output)
-        print()
-        # Gets and splits the hostname for the output file name.
-        hostname = net_connect.send_command("show ver | i uptime")
-        hostname = hostname.split()
-        hostname = hostname[0]
-        # Creates the file name with hostname and date and time.
-        fileName = hostname + "_" + dt_string
-        # Creates the text file in the result-config folder with the special name and writes to it.
-        backupFile = open("result-config/" + fileName + ".txt", "w+")
-        backupFile.write(output)
-        print("Outputted to " + fileName + ".txt")
-    # Handle an authentication error.
-    except (AuthenticationException, NetMikoAuthenticationException):
-        print("Login failed " + host)
-
-def get_saved_config_dns_tnet(host, username, password, enable_secret):
-    cisco_ios = {
-        'device_type': 'cisco_ios_telnet',
-        'host': host,
-        'username': username,
-        'password': password,
-        'secret': enable_secret,
-    }
-    # Creates the connection to the device.
-    try:
-        net_connect = ConnectHandler(**cisco_ios)
-        net_connect.enable()
-        # Configuring from commands in variable config file.
-        output = net_connect.send_config_from_file(conf_name)
-        time.sleep(0.5)
-        print()
-        print(output)
-        print()
-        # Creates the file name, which is the hostname, and the date and time.
-        fileName = ip + "_" + dt_string
-        # Creates the text file in the backup-config folder with the special name, and writes to it.
-        backupFile = open("result-config/" + fileName + ".txt", "w+")
-        backupFile.write(output)
-        print("Outputted to " + fileName + ".txt")
-    # Handle an authentication error.
-    except (AuthenticationException, NetMikoAuthenticationException):
-        print("Login failed " + host)
-
-# Open CSV file and ping test the hosts.
-def csv_option_host():
-    with open(csv_name, 'r') as csvfile:
-        csv_reader = reader(csvfile)
-        list_of_rows = list(csv_reader)
-        rows = len(list_of_rows)
-        while rows >= 2:
-            rows = rows - 1
-            ip = list_of_rows[rows][0]
-            ip_ping = ping(ip)
-            if ip_ping == None:
-                fileName = "downDevices_" + dt_string + ".txt"
-                downDeviceOutput = open("result-config/" + fileName, "a")
-                downDeviceOutput.write(str(ip) + "\n")
-                print(str(ip) + " is down!")
-            else:
-                get_saved_config_host(list_of_rows[rows][0], list_of_rows[rows][1], list_of_rows[rows][2], list_of_rows[rows][3])
-
-def csv_option_dns():
-    global ip # Exporting "ip" variable from csv_option_dns function to the global environment.
-    with open(csv_name, 'r') as csvfile:
-        csv_reader = reader(csvfile)
-        list_of_rows = list(csv_reader)
-        rows = len(list_of_rows)
-        while rows >= 2:
-            rows = rows - 1
-            ip = list_of_rows[rows][0]
-            ip_ping = ping(ip)
-            if ip_ping == None:
-                fileName = "downDevices_" + dt_string + ".txt"
-                downDeviceOutput = open("result-config/" + fileName, "a")
-                downDeviceOutput.write(str(ip) + "\n")
-                print(str(ip) + " is down!")
-            else:
-                get_saved_config_dns(list_of_rows[rows][0], list_of_rows[rows][1], list_of_rows[rows][2], list_of_rows[rows][3])
-
-def csv_option_host_tnet():
-    with open(csv_name, 'r') as csvfile:
-        csv_reader = reader(csvfile)
-        list_of_rows = list(csv_reader)
-        rows = len(list_of_rows)
-        while rows >= 2:
-            rows = rows - 1
-            ip = list_of_rows[rows][0]
-            ip_ping = ping(ip)
-            if ip_ping == None:
-                fileName = "downDevices_" + dt_string + ".txt"
-                downDeviceOutput = open("result-config/" + fileName, "a")
-                downDeviceOutput.write(str(ip) + "\n")
-                print(str(ip) + " is down!")
-            else:
-                get_saved_config_host_tnet(list_of_rows[rows][0], list_of_rows[rows][1], list_of_rows[rows][2], list_of_rows[rows][3])
-
-def csv_option_dns_tnet():
-    global ip # Exporting "ip" variable from csv_option_dns function to the global environment.
-    with open(csv_name, 'r') as csvfile:
-        csv_reader = reader(csvfile)
-        list_of_rows = list(csv_reader)
-        rows = len(list_of_rows)
-        while rows >= 2:
-            rows = rows - 1
-            ip = list_of_rows[rows][0]
-            ip_ping = ping(ip)
-            if ip_ping == None:
-                fileName = "downDevices_" + dt_string + ".txt"
-                downDeviceOutput = open("result-config/" + fileName, "a")
-                downDeviceOutput.write(str(ip) + "\n")
-                print(str(ip) + " is down!")
-            else:
-                get_saved_config_dns_tnet(list_of_rows[rows][0], list_of_rows[rows][1], list_of_rows[rows][2], list_of_rows[rows][3])
 
 # Define command arguments for inline options
-
 def main(argv):
     global conf_name
     global csv_name
     try:
         # Set host or DNS mode flag to Flase
         flag_host = False
-        opts, args = getopt.getopt(argv,"hnc:vs:",["conf=","csv=","help","host","verbose","ssh","tnet"])
+        opts, args = getopt.getopt(argv,"hnc:vs:",["conf=","csv=","help","host","verbose"])
     except getopt.GetoptError:
-        print(ver)
-        print()
-        print('Usage: conf_run.py -c <config_filename> -s <host_list.csv> (Opt --verbose)')
-        print('Note: Default output filename is DNS based (check README.md)')
-        print()
-        print('       -c, --conf <config_filename>')
-        print('       -s, --csv <host_list.csv>')
-        print('       Optional -v, --verbose')
-        print('       Optional -n, --host    Output filename use hostname retrived from device')
-        print('       Device connection method: --ssh (SSH: default), --tnet (telnet)')
-        print('       -h, --help    Print this help and exit')
+        print_help()
         sys.exit(2)
     for opt, arg in opts:
 #        if opt == ("-h"):
         if opt in ("-h", "--help"):
-            print(ver)
-            print()
-            print('Usage: conf_run.py -c <config_filename> -s <host_list.csv> (Opt --verbose)')
-            print('Note: Default output filename is DNS based (check README.md)')
-            print()
-            print('       -c, --conf <config_filename>')
-            print('       -s, --csv <host_list.csv>')
-            print('       Optional -v, --verbose')
-            print('       Optional -n, --host    Output filename use hostname retrived from device')
-            print('       Device connection method: --ssh (SSH: default), --tnet (telnet)')
-            print('       -h, --help    Print this help and exit')
+            print_help()
             sys.exit()
         elif opt in ("-c", "--conf"):
             conf_name = arg
@@ -272,30 +155,34 @@ def main(argv):
         # Choose if run in IP address or DNS mode. If no flag is set, default option is DNS mode.
         elif opt in ("-n", "--host"):
             flag_host = True
-    if flag_host == True:
-        print()
-        print("Output filename will use device hostname")
-        if opt in ("--ssh"):
-            print()
-            print("Running in SSH mode")
-            csv_option_host()
-        elif opt in ("--tnet"):
-            print()
-            print("Running in telnet mode")
-            csv_option_host_tnet()     
-    else:
-        print()
-        print("Output filename will be DNS based")
-        if opt in ("--ssh"):
-            print()
-            print("Running in SSH mode")
-            csv_option_dns()
-        elif opt in ("--tnet"):
-            print()
-            print("Running in telnet mode")
-            csv_option_dns_tnet()     
 
-if __name__ == "__main__":
+    #funzione che testa quali ip rispondono e crea un file con gli apparati down --> restituisce una lista di apparati up
+    active_devices = test_devices()
+
+    #ciclo sugli apparati UP: retrieve delle credenziali ed esecuzione dei comandi + backup config.
+    for ip in active_devices:
+        with open(csv_name, 'r') as csvfile:
+            csv_reader = csv.DictReader(csvfile)
+            #retrieve username/password
+            '''
+            Every row of csv file will look like this dictionary:
+
+                {
+                "IP": "10.0.0.1",
+                "Username": "admin",
+                "Password": "password",
+                "Enable Secret": "enable_secret_password"
+                }
+            '''
+            for row in csv_reader: #cerco la riga relativa all'apparato
+                if row['IP'] == ip: #abbiamo trovato l'entry, estraggo le info
+                    username = row['Username']
+                    password = row['Password']
+                    enable_secret =row['Enable Secret']
+                    get_saved_config(ip, username, password, enable_secret, flag_host)
+                    continue #esco dal ciclo interno di ricerca credenziali nel CSV, passo all'ip attivo successivo
+
+if __name__ == "__main__": #runs only from the command line
    main(sys.argv[1:])
    conf_name=''
    csv_name=''
